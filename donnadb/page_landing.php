a65b01f3a44c0cdc9db557a8e47ac46ab57f13fc
Dan Davies Brackett <dan@daviesbrackett.com>
2013-06-22 20:42:29 -0700

    restore nav to landing pages

--- donnadb/page_landing.php
+++ donnadb/page_landing.php
@@ -23,9 +23,9 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 //remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
 //remove_action( 'genesis_header', 'genesis_do_header' );
 //remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-remove_action( 'genesis_after_header', 'genesis_do_subnav', 15 );
-remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs');
+//remove_action( 'genesis_after_header', 'genesis_do_nav' );
+//remove_action( 'genesis_after_header', 'genesis_do_subnav', 15 );
+//remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs');
 remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
 //remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
 //remove_action( 'genesis_footer', 'genesis_do_footer' );
